27a9b9826ea00a67a6e1ddf01b5849bc706a7dce
michael <maydev99@gmail.com>
2020-11-04 17:54:31 -0500

    Disposables

--- app/src/main/java/com/bombadu/rxtime2/MainActivity.java
+++ app/src/main/java/com/bombadu/rxtime2/MainActivity.java
@@ -2,6 +2,7 @@ package com.bombadu.rxtime2;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,11 +10,18 @@ import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private TextView text;
+     //vars
+    private CompositeDisposable disposables = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
          * 2. Apply an operator to the Observable
          * 3. Designate what thread to do the work on and what thread to emit the results to
          * 4. Subscribe an Observer to the Observable and view the results
+         *
+         * If using MVVM then use disposables in ViewModel
          */
 
         Observable<Task> taskObservable = Observable //Create Observable
@@ -48,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSubscribe(@NonNull Disposable d) {
                 Log.d("TAG", "onSubscribe: called.");
+                disposables.add(d);
             }
 
             @Override
@@ -69,5 +80,19 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        disposables.add(taskObservable.subscribe(new Consumer<Task>() {
+            @Override
+            public void accept(Task task) throws Exception {
+
+            }
+        }));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposables.clear();
+        //disposables.dispose(); hard clear, not used as much
     }
 }
