886e4ea548155bcc1bda61b4622ee3030dd11b53
michael <maydev99@gmail.com>
2020-11-06 09:33:47 -0500

    Types of Transformation Operators

--- app/src/main/java/com/bombadu/rxtime2/MainActivity.java
+++ app/src/main/java/com/bombadu/rxtime2/MainActivity.java
@@ -15,9 +15,14 @@ import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 /*
-    Take - Outputs only 3 of the items in Task
-    TakeWhile - like a while loop, outputs items until a condition is met
-        in this case returns only the first item because the second item is false
+   Operators that transform items that are emitted by an observable
+   Types:
+   Map()
+   Buffer()
+   Debounce()
+   FlatMap()
+   ConcatMap()
+   SwitchMap()
  */
 
 public class MainActivity extends AppCompatActivity {
@@ -30,45 +35,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable<Task> taskObservable = Observable //**TakeWhile
-                .fromIterable(DataSource.createTasksList())
-                .takeWhile(new Predicate<Task>() {
-                    @Override
-                    public boolean test(@NonNull Task task) throws Exception {
-                        return task.isComplete();
-                    }
-                })
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
-
-        /*Observable<Task> taskObservable = Observable   **Take
-                .fromIterable(DataSource.createTasksList())
-                .take(3) //Three
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());*/
-
-        taskObservable.subscribe(new Observer<Task>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
-
-            }
-
-            @Override
-            public void onNext(@NonNull Task task) {
-                Log.d(TAG, "onNext: " + task.getDescription());
-
-            }
-
-            @Override
-            public void onError(@NonNull Throwable e) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        });
 
 
     }
