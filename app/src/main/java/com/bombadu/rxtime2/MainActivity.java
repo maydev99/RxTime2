8c7f7c190368a85b6968abdaa07541f48e6f091e
michael <maydev99@gmail.com>
2020-11-06 15:38:16 -0500

    Transformation Operators - FlatMap  -Transform the item(s) emitted by an Observable into Observables,then flatten the emissions from those into a single Observable.  -If you're familiar with LiveData, MediatorLiveData can do something very similar.
    -Probably uses other operator in conjunction with flat map
    -Does not emit the objects in the same order (Randomly)
    
    -Added Dependencies:
      RxCallAdapter
      RecyclerView
    
    -Uses Whartons RxBinding library

--- app/src/main/java/com/bombadu/rxtime2/MainActivity.java
+++ app/src/main/java/com/bombadu/rxtime2/MainActivity.java
@@ -2,87 +2,167 @@ package com.bombadu.rxtime2;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.jakewharton.rxbinding3.view.RxView;
+import java.util.List;
+import java.util.Random;
 
-import java.util.concurrent.TimeUnit;
-
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
-import kotlin.Unit;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
 
 /*
-   Transformation Operators - ThrottleFirst
-    Reduces button spamming
-    RxView make button click observable
-    Example limit registering button click to every 4 seconds
+   Transformation Operators - FlatMap
+    Transform the item(s) emitted by an Observable into Observables,
+    then flatten the emissions from those into a single Observable.
+    If you're familiar with LiveData, MediatorLiveData can do something very similar.
+
+    Probably use other operator in conjunction with flat map
+    Does not emit the objects in the same order (Randomly)
+
+    Added Dependencies:
+    RxCallAdapter
+    RecyclerView
 
     Uses Whartons RxBinding library
  */
 
 public class MainActivity extends AppCompatActivity {
+
     private static final String TAG = "MainActivity";
 
     //ui
-    private Button button;
+    private RecyclerView recyclerView;
 
-    //vars
+    // vars
     private CompositeDisposable disposables = new CompositeDisposable();
-    private long timeSinceLastRequest; //for printouts only. Not part of Logic.
+    private RecyclerAdapter adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recycler_view);
 
-        button = findViewById(R.id.button);
-        timeSinceLastRequest = System.currentTimeMillis();
+        initRecyclerView();
 
-        //set a click listener to the button with RxBinding Library
-        RxView.clicks(button)
-                .throttleFirst(4000,TimeUnit.MILLISECONDS) //Throttles clicks so 4000 ms pass before registering another click
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<Unit>() {
+        getPostsObservable()
+                .subscribeOn(Schedulers.io())
+                .flatMap(new Function<Post, ObservableSource<Post>>() {
                     @Override
-                    public void onSubscribe(@NonNull Disposable d) {
+                    public ObservableSource<Post> apply(Post post) throws Exception {
+                        return getCommentsObservable(post);
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Post>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
                         disposables.add(d);
                     }
 
                     @Override
-                    public void onNext(@NonNull Unit unit) {
-                        Log.d(TAG, "onNext: time since last clicked: " + (System.currentTimeMillis() - timeSinceLastRequest));
-                        someMethod();
+                    public void onNext(Post post) {
+                        updatePost(post);
                     }
 
                     @Override
-                    public void onError(@NonNull Throwable e) {
-
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "onError: ", e);
                     }
 
                     @Override
                     public void onComplete() {
-
                     }
                 });
     }
 
-    private void someMethod() {
-        timeSinceLastRequest = System.currentTimeMillis();
-        //do something
-        Toast.makeText(this, "you clicked the button!", Toast.LENGTH_SHORT).show();
+    private Observable<Post> getPostsObservable(){
+        return ServiceGenerator.getRequestAPI()
+                .getPosts()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMap(new Function<List<Post>, ObservableSource<Post>>() {
+                    @Override
+                    public ObservableSource<Post> apply(final List<Post> posts) throws Exception {
+                        adapter.setPosts(posts);
+                        return Observable.fromIterable(posts)
+                                .subscribeOn(Schedulers.io());
+                    }
+                });
+    }
+
+    private void updatePost(final Post p){
+        Observable
+                .fromIterable(adapter.getPosts())
+                .filter(new Predicate<Post>() {
+                    @Override
+                    public boolean test(Post post) throws Exception {
+                        return post.getId() == p.getId();
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Post>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposables.add(d);
+                    }
+
+                    @Override
+                    public void onNext(Post post) {
+                        Log.d(TAG, "onNext: updating post: " + post.getId() + ", thread: " + Thread.currentThread().getName());
+                        adapter.updatePost(post);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "onError: ", e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                    }
+                });
+    }
+
+    private Observable<Post> getCommentsObservable(final Post post){
+        return ServiceGenerator.getRequestAPI()
+                .getComments(post.getId())
+                .map(new Function<List<Comment>, Post>() {
+                    @Override
+                    public Post apply(List<Comment> comments) throws Exception {
+
+                        int delay = ((new Random()).nextInt(5) + 1) * 1000; // sleep thread for x ms
+                        Thread.sleep(delay);
+                        Log.d(TAG, "apply: sleeping thread " + Thread.currentThread().getName() + " for " + String.valueOf(delay)+ "ms");
+
+                        post.setComments(comments);
+                        return post;
+                    }
+                })
+                .subscribeOn(Schedulers.io());
+
+    }
+
+    private void initRecyclerView(){
+        adapter = new RecyclerAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        disposables.clear(); //Dispose Observable
+        disposables.clear();
     }
 }
--- app/src/main/java/com/bombadu/rxtime2/RequestAPI.java
+++ app/src/main/java/com/bombadu/rxtime2/RequestAPI.java
@@ -1,11 +1,20 @@
 package com.bombadu.rxtime2;
 
+import java.util.List;
+
 import io.reactivex.Flowable;
+import io.reactivex.Observable;
 import okhttp3.ResponseBody;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface RequestAPI {
 
-    @GET("todos/1")
-    Flowable<ResponseBody> makeQuery();
+    @GET("posts")
+    Observable<List<Post>> getPosts();
+
+    @GET("posts/{id}/comments")
+    Observable<List<Comment>> getComments(
+            @Path("id") int id
+    );
 }
